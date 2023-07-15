99af3fc8814e89dc323be3fbaaa40b490dcbe61c
Florian Rädiker <git@flra.eu>
2023-07-15 20:29:51 +0200

    Add Tailwind and Prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from "vite"
 import preact from "@preact/preset-vite"
+import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true,
+  },
 })
