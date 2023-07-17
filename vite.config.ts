23bfec25aa417e99e05b9e88873f7fc36ebc7124
Florian Rädiker <git@flra.eu>
2023-07-17 13:49:39 +0200

    💅 Add favicon

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
+import { faviconsPlugin } from "@darkobits/vite-plugin-favicons"
 import preact from "@preact/preset-vite"
 import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [
+    preact(),
+    faviconsPlugin({
+      icons: {
+        favicons: {
+          source: "./public/no_scooter.png",
+        },
+      },
+    }),
+  ],
   server: {
     port: 3000,
     strictPort: true,
